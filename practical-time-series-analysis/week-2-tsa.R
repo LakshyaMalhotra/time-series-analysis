93e86a4cf2d4a77963755004ffc0666764318ac5
LakshyaMalhotra <lakshya9009@gmail.com>
2021-04-27 16:47:17 -0700

    Added till auto-covariance lecture.

---
+++ practical-time-series-analysis/week-2-tsa.R
@@ -0,0 +1,37 @@
+#-----
+# Some time plots
+require(astsa)
+help(astsa)
+# Johnson & Johnson data
+help(jj)
+plot(jj, type='o', main='Johnson&Johnson quarterly earnings per share', 
+     ylab='Earnings', xlab='Quarters')
+
+# Flu data
+help(flu)
+plot(flu, main='Monthly Pneumonia and Influenza Deaths in US', 
+     ylab='Number of deaths per 10000 people', xlab='Months')
+
+# Global Temperature deviations data
+help(globtemp)
+plot(globtemp, main='Global mean land-ocean deviations from average temp of 1951-1980', 
+     ylab='Temperature deviations', type='o', xlab='Years')
+
+# Global Temperature deviations data (land only)
+help(globtempl)
+plot(globtempl, main='Global mean land deviations from average temp of 1951-1980', 
+     ylab='Temperature deviations', type='o', xlab='Years')
+
+# Variable star data
+help(star)
+plot(star, main='Magnitude of a start taken at midnight for 600 consecutive days', 
+     ylab='Magnitude', xlab='Days')
+
+#-----
+# ## Intuition about stationarity
+# - Weak Stationary time-series means no systematic change in the mean, i.e. No trend 
+# - No systematic change in the variation
+# - No periodic fluctuation
+
+#-----
+# ## Auto-covariance function
