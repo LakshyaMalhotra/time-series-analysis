4e8b3250e5276de43d8fca8fcca116b2ec01406a
LakshyaMalhotra <lakshya9009@gmail.com>
2021-04-28 18:43:28 -0700

    Added stationarity, first examples.

---
+++ practical-time-series-analysis/week-3-tsa.R
@@ -0,0 +1,6 @@
+#-----
+# Able to explain why stationarity is crucial in formulating a model from the data
+# ## Stationarity: first examples
+# 1. White Noise: stationary
+# 2. Random walks: non-stationary
+# 3. Moving averages: stationary
